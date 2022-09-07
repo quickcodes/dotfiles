6bb4b9e7a17bb92a13cc5c3d56e5c48304d4e1c7
Dhruv Soni <81878722+quickcodes@users.noreply.github.com>
2022-09-07 17:38:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-yay -S nitrogen picom-git nitrogen youtube-dl mpv npm peerflix fzf tty-clock btop cbonsai acpi vlc foremost sublime-text-4 
+yay -S nitrogen picom-git youtube-dl mpv npm peerflix fzf tty-clock btop cbonsai acpi vlc foremost sublime-text-4 
 
